85dc8c54b9ab13449a41f751e1306f4d01f1c84f
Georg Treitz <georg.treitz@gmail.com>
2021-01-05 12:33:42 +0100

    calculation moved to seperate function

--- components/Calc.js
+++ components/Calc.js
@@ -5,29 +5,66 @@ import InputComp from './InputComp';
 const Calc = () => {
     const [inputs, setInputs] = useState([0]);
     const [rabatt, setRabatt] = useState(0);
-    const [values, setValues] = useState({ pic0: "" });
-    const [rahmen, setRahmen] = useState({ rahmen0: "" });
-    // console.log(inputs)
-    // const removeItem = (target) => {
-    //     setInputs(inputs.filter(ele => ele != target.name))
-    //     console.log("Inputs: ", inputs)
-    //     console.log("Count: ", count)
-    // }
+    const [values, setValues] = useState({ pic0: "0" });
+    const [frame, setFrame] = useState({ frame0: "0" });
+    const [mwSt, setMwSt] = useState("07");
+    const [result, setResult] = useState({
+        total: 0,
+        picNetto: 0,
+        picMwSt: 0,
+        frameNetto: 0,
+        frameMwSt: 0,
+        mwStTotal: 0,
+        nettoTotal: 0,
+    });
     const handleAdd = () => {
-        // const name
         setInputs(state => [...state, inputs.length])
         setValues(prevState => ({
             ...prevState,
-            [`pic${inputs.length}`]: ""
+            [`pic${inputs.length}`]: "0"
         }))
-        setRahmen(prevState => ({
+        setFrame(prevState => ({
             ...prevState,
-            [`rahmen${inputs.length}`]: ""
+            [`frame${inputs.length}`]: "0"
         }))
     }
+    useEffect(() => {
+        calcAll()
+    }, [inputs, rabatt, values, frame, mwSt]);
+    const calcAll = () => {
+        // Bild berechnen
+        const picNetto = ((Object.values(values)).reduce((prev, curr) => (parseFloat(prev) + ((parseFloat(curr) / `1.${mwSt}`).toFixed(2) * 1)), 0)).toFixed(2)
+        const picMwSt = ((Object.values(values)).reduce((prev, curr) => prev * 1 + curr * 1) - ((Object.values(values)).reduce((prev, curr) => prev * 1 + curr * 1) / `1.${mwSt}`)).toFixed(2)
+        const picTotal = (picNetto * 1 + picMwSt * 1).toFixed(2)
 
+        // Rahmen berechnen
+        const frameNetto = ((Object.values(frame)).reduce((prev, curr) => (parseFloat(prev) + ((parseFloat(curr) / `1.${mwSt}`).toFixed(2) * 1)), 0)).toFixed(2)
+        const frameMwSt = ((Object.values(frame)).reduce((prev, curr) => prev * 1 + curr * 1) - ((Object.values(frame)).reduce((prev, curr) => prev * 1 + curr * 1) / `1.${mwSt}`)).toFixed(2)
+        const frameTotal = (frameNetto * 1 + frameMwSt * 1).toFixed(2)
+
+        // Zusammenführen
+
+        const mwStTotal = (picMwSt * 1 + frameMwSt * 1).toFixed(2)
+        const nettoTotal = (picNetto * 1 + frameNetto * 1).toFixed(2)
+        const total = (nettoTotal * 1 + mwStTotal * 1).toFixed(2)
+        setResult({
+            total,
+            picNetto,
+            picMwSt,
+            frameNetto,
+            frameMwSt,
+            mwStTotal,
+            nettoTotal,
+        })
+    }
     return (
         <main>
+            <span> Steuersatz:</span>
+            <select onChange={({ target }) => setMwSt(target.value)} value={mwSt}>
+                <option value="05">5%</option>
+                <option value="07" defaultValue>7%</option>
+                <option value="19">19%</option>
+            </select>
             <div className="grid center">
                 <button onClick={() => handleAdd()}>Add</button>
                 <div>Brutto</div>
@@ -44,19 +81,19 @@ const Calc = () => {
                         onChange={({ target }) => setValues(state => ({ ...state, [target.name]: target.value }))}
                         value={values[`pic${ele}`]}
                     />
-                    <div className="ergebnis">{(values[`pic${ele}`] / 1.05).toFixed(2)} €</div>
-                    <div className="ergebnis">{(values[`pic${ele}`] - (values[`pic${ele}`] / 1.05)).toFixed(2)} €</div>
+                    <div className="ergebnis">{(values[`pic${ele}`] / `1.${mwSt}`).toFixed(2)} €</div>
+                    <div className="ergebnis">{(values[`pic${ele}`] - (values[`pic${ele}`] / `1.${mwSt}`)).toFixed(2)} €</div>
                 </div>
                 <div className="grid">
                     <span>Rahmen {ele + 1}:</span>
                     <input
                         type="number"
-                        name={`rahmen${ele}`}
-                        onChange={({ target }) => setRahmen(state => ({ ...state, [target.name]: target.value }))}
-                        value={rahmen[ele]}
+                        name={`frame${ele}`}
+                        onChange={({ target }) => setFrame(state => ({ ...state, [target.name]: target.value }))}
+                        value={frame[ele]}
                     />
-                    <div className="ergebnis">{(rahmen[`rahmen${ele}`] / 1.05).toFixed(2)} €</div>
-                    <div className="ergebnis">{(rahmen[`rahmen${ele}`] - (rahmen[`rahmen${ele}`] / 1.05)).toFixed(2)} €</div>
+                    <div className="ergebnis">{(frame[`frame${ele}`] / `1.${mwSt}`).toFixed(2)} €</div>
+                    <div className="ergebnis">{(frame[`frame${ele}`] - (frame[`frame${ele}`] / `1.${mwSt}`)).toFixed(2)} €</div>
                 </div>
             </div>
             )}
@@ -64,11 +101,14 @@ const Calc = () => {
 
             <div className="grid">
                 <div>Gesammt:</div>
-                <div className="ergebnis">{((((Object.values(values)).reduce((prev, curr) => prev * 1 + curr * 1)) * 1).toFixed(2))} €</div>
+                {/* <div className="ergebnis">{((((Object.values(values)).reduce((prev, curr) => prev * 1 + curr * 1)) * 1).toFixed(2))} €</div> */}
+                <div className="ergebnis">{
+                    result.total
+                } €</div>
 
-                <div className="ergebnis">{((Object.values(values)).reduce((prev, curr) => (parseFloat(prev) + ((parseFloat(curr) / 1.05).toFixed(2) * 1)), 0))} €</div>
+                <div className="ergebnis">{result.nettoTotal} €</div>
 
-                <div className="ergebnis">{((Object.values(values)).reduce((prev, curr) => prev * 1 + curr * 1) - ((Object.values(values)).reduce((prev, curr) => prev * 1 + curr * 1) / 1.05)).toFixed(2)} €</div>
+                <div className="ergebnis">{result.mwStTotal} €</div>
             </div>
             <span className="text">Rabatt:</span><input type="number" name="rabatt" onChange={({ target }) => setRabatt(target.value)} value={rabatt} />
 
